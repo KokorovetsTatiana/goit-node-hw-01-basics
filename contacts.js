ef20d5581c80574e20c4584810c7a98701a12bc9
KokorovetsTatiana <tanuska@ukr.net>
2021-09-12 23:29:31 +0300

    created main functions: listContacts, getContactbyId, removeContact, addContact

--- contacts.js
+++ contacts.js
@@ -1,16 +1,50 @@
 const fs = require("fs/promises");
 const path = require("path");
-// const { v4 } = require("uuid");
+const { v4 } = require("uuid");
+const contacts = require("./db/contacts.json")
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join("./db", "contacts.json");
 
-// const updateContacts = async (newContacts) => {
-//     await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-// };
+const updateContacts = async(data) => {
+    await fs.writeFile(contactsPath, JSON.stringify(data));
+    }
 
-// module.exports = updateContacts;
+const listContacts = async ()  => contacts;
 
-fs.readFile(contactsPath, (error, data) => {
-    console.log(error);
-    console.log(data);
-})
+const getContactById = async(contactId) => {
+    const contacts = await listContacts();
+    const contact = contacts.find(item => item.id === contactId);
+    if (!contact) {
+        console.log(`Contact with id=${contactId} not found`)
+    }
+    return contact;
+};
+
+const removeContact = async(contactId) => {
+    const contacts = await listContacts();
+    const idRemove = contacts.findIndex(item => item.id === contactId);
+    if (idRemove === -1) {
+        throw new Error(`Contact with id=${contactId} not found`)
+    }
+    contacts.splice(idRemove, 1)
+        await updateContacts(contacts);
+        return `Contact with id=${contactId} has been successfully removed`;
+    };
+
+const addContact = async (data) => {
+    const contacts = await listContacts();
+    const newContact = { ...data, id: v4() };
+    contacts.push(newContact);
+    await updateContacts(contacts);
+    return newContact;
+};
+
+const contactsOperations = {
+    updateContacts,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
+
+module.exports = contactsOperations;
--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-const fs = require("fs/promises");
-// const path = require("path");
-// const { v4 } = require("uuid");
+const contactsOperations = require("../goit-node-hw-01-basics/contacts");
 
-// const contactsPath = path.join("./db", "contacts.json");
+const workWithContacts = async(type = "listContacts", contactId, data) => {
+    try {
+        switch (type) {
+            case "listContacts":
+                return await contactsOperations.listContacts();
+            case "getContactById":
+                return await contactsOperations.getContactById(contactId);
+            case "removeContact":
+                return await contactsOperations.removeContact(contactId);
+            case "addContact":
+                return await contactsOperations.addContact(data);
+        }
+    }
+    catch (error) {
+        throw error;
+    }
+}
 
-// const updateContacts = async (newContacts) => {
-//     await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-// };
+const contactId = 2;
+const data = {
+    "id": 2,
+    "name": "Chaim Lewis",
+    "email": "dui.in@egetlacus.ca",
+    "phone": "(294) 840-6685"
+}
 
-// module.exports = updateContacts;
+// workWithContacts("listContacts")
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
 
-fs.readFile("db/text.txt", (error, data) => {
-    // if (error) {
-    //     throw new Error("cannot read file")
-    // }
-    // const text = data.toString();
-    console.log(error);
-    console.log(data);
-})
+// workWithContacts("getContactById", contactId)
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error));
+
+// workWithContacts("removeContact", contactId)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+workWithContacts("addContact", "", data)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
